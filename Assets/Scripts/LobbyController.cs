f2e2d1a1b7b744c0f1ea1c7d2af35fdeaae2cba7
Cornelius David Herianto <cornelius.david97@gmail.com>
2017-10-18 07:30:30 +0700

    update server

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -3,42 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LobbyController : MonoBehaviour {
-	
+public class LobbyController : MonoBehaviour
+{
+
 	public NetworkController netCon;
-	public List<PlayerLobby> list = new List<PlayerLobby>();
+	public List<PlayerLobby> list;
 	PlayerLobby curPlayer;
 	Player playerTemp;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		netCon = GameObject.FindObjectOfType<NetworkController>();
 		playerTemp = GameObject.FindObjectOfType<Player>();
 
+		netCon.currentPlayer = playerTemp;
 		Debug.Log(netCon);
 		netCon.Connect();
-		
-		netCon.currentPlayer = playerTemp;
-		netCon.userJoinLobby(playerTemp.GetComponent<Player>());
-
-		List<Player> players = new List<Player>();
-		players.Add(playerTemp);
-
-		for (int i = 0; i < players.Count; i++)
-		{
-			list[i].transform.gameObject.SetActive(true);
-			list[i].nameText.text = players[i].playerName;
-			if (players[i] != playerTemp)
-			{
-				list[i].readyBtn.gameObject.SetActive(false);
-			}
-			else
-			{
-				list[i].readyBtn.onClick.AddListener(onReadyClicked);
-				curPlayer = list[i];
-			}
-		}
-
 	}
 
 	private void onReadyClicked()
@@ -49,17 +30,20 @@ public class LobbyController : MonoBehaviour {
 
 
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
 
-	public void onUserConnected(List<Player> players)
+	public void onUserConnected(List<string> names)
 	{
-		for (int i = 0; i < players.Count; i++)
+		Debug.Log("test");
+		for (int i = 0; i < names.Count; i++)
 		{
-			list[i].transform.gameObject.SetActive(true);
-			list[i].nameText.text = players[i].playerName;
-			if (players[i] != playerTemp)
+			list[i].gameObject.SetActive(true);
+			list[i].nameText.text = names[i];
+			Debug.Log(names[i]);
+			if (names[i] != playerTemp.playerName)
 			{
 				list[i].readyBtn.gameObject.SetActive(false);
 			}
